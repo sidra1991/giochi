cdd1a12e50b35c14c7eab1f0e52a29b358f16cd8
sidra1991 <diomede1991@libero.it>
2022-08-23 16:50:01 +0200

    base home

---
+++ platform/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/stile-home.css">
+    <title>Document</title>
+</head>
+<body>
+    <main class="m-color">
+        <div class="menu-home" >    
+            <div class="button-home" >
+                <a href="pages/index-game.php"> 
+                    <i class="fa-solid fa-dice"></i>
+                </a>
+            </div>
+
+            <div class="button-home" >
+                <a href="pages/options.php"> 
+                    <i class="fa-solid fa-gears"></i>
+                </a>
+            </div>
+        </div>    
+    </main>
+
+    <script src="https://kit.fontawesome.com/dbdf1a8f31.js" crossorigin="anonymous"></script>
+</body>
+</html>
