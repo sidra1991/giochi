6581709130f1c0923af625219dd1386454310733
sidra1991 <diomede1991@libero.it>
2022-08-24 12:16:29 +0200

    pausa

--- platform/pages/game/scopa/vs-bot.php
+++ platform/pages/game/scopa/vs-bot.php
@@ -10,8 +10,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="stiles/play-stile.css">
+    <link rel="stylesheet" href="../../../style/general.css">
 </head>
 <body>
-    
+    <div class="container m-color" >
+        <div class="table" >
+            <div class="table-card" >
+            </div>
+        </div>
+    </div>
 </body>
 </html>
